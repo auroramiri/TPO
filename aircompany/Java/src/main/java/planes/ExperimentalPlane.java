4418b5d185782b8ccec20260ac3b2929898b6bfc
auroramiri <43730173+auroramiri@users.noreply.github.com>
2024-05-04 13:39:43 +0300

    Add files via upload

---
+++ aircompany/Java/src/main/java/planes/ExperimentalPlane.java
@@ -0,0 +1,30 @@
+package planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalTypes;
+
+public class ExperimentalPlane extends Plane {
+
+    private ClassificationLevel classificationLevel;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity,
+                             ExperimentalTypes type, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ClassificationLevel getClassificationLevel() {
+        return classificationLevel;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel) {
+        this.classificationLevel = classificationLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "experimentalPlane{" +
+                "model='" + this.getModel() + '\'' +
+                '}';
+    }
+}
